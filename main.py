f57cad1f2192c94da34b41fe331ce2128044fd6b
Lahiru-Ima <lahiru.sl24@gmail.com>
2023-01-02 09:48:30 +0530

    Remove get method & fix erros

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-
+from typing import List
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -15,34 +14,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-class Question(BaseModel):
-    question1:int 
-    question2:int
-    question3:int
-    question4:int
-    question5:int
-    question6:int
-    question7:int
-    question8:int
-    question9:int
-    question10:int
-    question11:int
-    question12:int
-    question13:int
-    question14:int
-    question15:int
-    question16:int
-    question17:int
-    question18:int
+class Answers(BaseModel):
+    id: int
+    question: str
+    options: List[str] = []
 
 def prediction(lst):
     model = joblib.load("dep_predict_model.pickle")
-    # filename = 'dep_predict_model.pickle'
-    # with open(filename,'rb') as file:
-    #     model = pickle.load(file)
     pred_value = model.predict([lst])
-    # return pred_value
     if pred_value == [4]:
             return 'Extremely Severe'
     elif pred_value == [3]:
@@ -56,13 +35,7 @@ def prediction(lst):
     else:
         return 'None'
 
-
 @app.post("/")
-def get_question(question:Question):
-    answers =  question.dict()
-    feature_list = [answer for answer in answers.values()]
-    pred_value = prediction(feature_list)
+def create_question(answers: List[int]):
+    pred_value = prediction(answers)
     return pred_value
-
-# if __name__ == "__main__":
-#     uvicorn.run(app,host='127.0.0.1',port=8000)
